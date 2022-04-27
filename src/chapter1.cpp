58244e43269ba409082da7295e8d4122e1690fa9
Mikael Andersson <qubex2@gmail.com>
2022-04-27 19:48:56 +0200

    fixes

--- src/chapter1.cpp
+++ src/chapter1.cpp
@@ -14,7 +14,21 @@ int main() {
 
     int pii = static_cast<int>(pi);
 
-    std::cout << std::format("PII: {}\n", pii) << std::endl;
+    std::cout << std::format("PII: {}\n", pii);
 
     std::cout << std::format("infinity: {}\n", std::numeric_limits<double>::infinity);
+
+    std::array arr {1, 2, 3, 4, 5, 6, 7 ,8};
+    std::cout << std::format("array length: {}\n", arr.size());
+
+    std::array arr2 {10, 11, 12};
+
+    auto [x, y, z] { arr2 };
+
+    std::cout << std::format("x: {}, y: {}, z: {}\n", x, y, z);
+
+    for(std::array an {1, 2, 3, 4, 5, 6, 7, 8}; auto a : an) {
+        std::cout << std::format("number: {}\n", a);
+    }
+
 }
